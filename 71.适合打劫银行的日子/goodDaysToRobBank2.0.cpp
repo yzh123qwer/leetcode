f150a03ceda474e6869f1dd209f09fc08228e0a1
yzh123qwer <91482200+yzh123qwer@users.noreply.github.com>
2022-03-06 21:11:05 +0800

    中等题

--- 71.适合打劫银行的日子/goodDaysToRobBank2.0.cpp
+++ 71.适合打劫银行的日子/goodDaysToRobBank2.0.cpp
@@ -1,51 +1,23 @@
 class Solution {
 public:
     vector<int> goodDaysToRobBank(vector<int>& security, int time) {
+        int n = security.size();
+        vector<int> left(n);
+        vector<int> right(n);
+        for (int i = 1; i < n; i++) {
+            if (security[i] <= security[i - 1]) {
+                left[i] = left[i - 1] + 1;
+            }
+            if (security[n - i - 1] <= security[n - i]) {
+                right[n - i - 1] = right[n - i] + 1;
+            }
+        }
+
         vector<int> ans;
-        if (time == 0){
-            for (int i = 0; i < security.size(); i++)
-                ans.push_back(i);
-            return ans;
-        }
-        if (time * 2 > security.size())
-            return ans;
-        int time_temp;
-        bool S;
-        if (time == 1){
-            for (int i = time; i < security.size() - 1; i++){
-                S = true;
-                if (security[i + 1] < security[i])
-                    S = false;
-                if (security[i - 1] < security[i])
-                    S = false;
-                if (S == true)
-                    ans.push_back(i);
+        for (int i = time; i < n - time; i++) {
+            if (left[i] >= time && right[i] >= time) {
+                ans.emplace_back(i);
             }
-            return ans;
-        }
-        for (int i = time; i < security.size() - time; i++){
-            S = true;
-            for (int j = i, time_temp = time; time_temp != 0; time_temp--, j++){
-                if (security[j + 1] >= security[j])
-                    continue;
-                else {
-                    S = false;
-                    break;
-                }
-            }
-            if (S == false)
-                continue;
-            for (int j = i, time_temp = time; time_temp != 0; time_temp--, j--){
-                if (security[j - 1] >= security[j])
-                    continue;
-                else {
-                    S = false;
-                    break;
-                }
-            }
-            if (S == false)
-                continue;
-            ans.push_back(i);
         }
         return ans;
     }
