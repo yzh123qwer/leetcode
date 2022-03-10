1874c6e3c23e273072ccd3f467d53bad4418e8d2
yzh123qwer <91482200+yzh123qwer@users.noreply.github.com>
2022-03-10 12:49:18 +0800

    简单题

---
+++ 76.N 叉树的前序遍历/preorder.cpp
@@ -0,0 +1,53 @@
+/*
+589. N 叉树的前序遍历
+给定一个 n 叉树的根节点  root ，返回 其节点值的 前序遍历 。
+
+n 叉树 在输入中按层序遍历进行序列化表示，每组子节点由空值 null 分隔（请参见示例）。
+
+
+示例 1：
+
+
+
+输入：root = [1,null,3,2,4,null,5,6]
+输出：[1,3,5,6,2,4]
+示例 2：
+
+
+
+输入：root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+输出：[1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+*/
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> preorder(Node* root) {
+        if (root == nullptr){
+            return ans;
+        }
+        else {
+            ans.push_back(root->val);
+            for (auto i : root->children)
+                preorder(i);
+            return ans;
+        }
+    }
+};
