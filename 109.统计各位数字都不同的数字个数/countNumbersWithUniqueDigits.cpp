3f93fe045477f130ccecc71471aae54ea5ba7350
yzh123qwer <91482200+yzh123qwer@users.noreply.github.com>
2022-04-11 09:24:12 +0800

    中等题

---
+++ 109.统计各位数字都不同的数字个数/countNumbersWithUniqueDigits.cpp
@@ -0,0 +1,32 @@
+/*
+357. 统计各位数字都不同的数字个数
+给你一个整数 n ，统计并返回各位数字都不同的数字 x 的个数，其中 0 <= x < 10n 。
+ 
+
+示例 1：
+
+输入：n = 2
+输出：91
+解释：答案应为除去 11、22、33、44、55、66、77、88、99 外，在 0 ≤ x < 100 范围内的所有数字。 
+示例 2：
+
+输入：n = 0
+输出：1
+*/
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 10;
+        }
+        int ans = 10, cur = 9;
+        for (int i = 0; i < n - 1; ++i) {
+            cur *= 9 - i;
+            ans += cur;
+        }
+        return ans;
+    }
+};
