6d9cdb6403345a23954e31969f7602f426b3a647
yzh123qwer <91482200+yzh123qwer@users.noreply.github.com>
2022-06-29 14:44:57 +0800

    中等题(URL的压缩和解压)

---
+++ 147.TinyURL 的加密与解密/URL.cpp
@@ -0,0 +1,56 @@
+/*
+535. TinyURL 的加密与解密
+TinyURL 是一种 URL 简化服务， 比如：当你输入一个 URL https://leetcode.com/problems/design-tinyurl 时，它将返回一个简化的URL http://tinyurl.com/4e9iAk 。请你设计一个类来加密与解密 TinyURL 。
+
+加密和解密算法如何设计和运作是没有限制的，你只需要保证一个 URL 可以被加密成一个 TinyURL ，并且这个 TinyURL 可以用解密方法恢复成原本的 URL 。
+
+实现 Solution 类：
+
+Solution() 初始化 TinyURL 系统对象。
+String encode(String longUrl) 返回 longUrl 对应的 TinyURL 。
+String decode(String shortUrl) 返回 shortUrl 原本的 URL 。题目数据保证给定的 shortUrl 是由同一个系统对象加密的。
+ 
+
+示例：
+
+输入：url = "https://leetcode.com/problems/design-tinyurl"
+输出："https://leetcode.com/problems/design-tinyurl"
+
+解释：
+Solution obj = new Solution();
+string tiny = obj.encode(url); // 返回加密后得到的 TinyURL 。
+string ans = obj.decode(tiny); // 返回解密后得到的原本的 URL 。
+*/
+class Solution {
+    int id;
+    unordered_map<int, string> hash;
+    string fun(string s){
+        int temp = 0;
+        for (int i = 0; i < s.size(); i++)
+            temp = temp * 10 + (s[i] - '0');
+        return hash.find(temp)->second;
+    }
+public:
+    Solution (){
+        id = 0;
+    }
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        id++;
+        hash.insert(make_pair(id, longUrl));
+        string temp = "www." + to_string(id);
+        return temp;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        string temp;
+        for (int i = 4; i < shortUrl.size(); i++)
+            temp.push_back(shortUrl[i]);
+        return fun(temp);
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
